f10509ce8024b4169a77ff17275be38a216a7c4c
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-01 08:06:27 +0200

    Export inline-source to an external npm module

--- services/webapp/config-overrides.js
+++ services/webapp/config-overrides.js
@@ -2,29 +2,14 @@
 
 const rewireEslint = require('react-app-rewire-eslint')
 const rewireStylus = require('react-app-rewire-stylus-modules')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const rewireInlinSource = require('react-app-rewire-inline-sourcep')
 
 module.exports = function override (config, env) {
     config = rewireEslint(config, env)
     config = rewireStylus(config, env)
-
-    // Inline CSS & Javascript
-    if (env === 'production') {
-        let HtmlWebpackPluginIdx = null
-        config.plugins.map((plugin, idx) => {
-            if (plugin.options && plugin.options.template) {
-                HtmlWebpackPluginIdx = idx
-                plugin.options.inlineSource = '.(css)$' // '.(js|css)$'
-            }
-            return plugin
-        })
-
-        if (HtmlWebpackPluginIdx === null) {
-            config.plugins.push(new HtmlWebpackInlineSourcePlugin())
-        } else {
-            config.plugins.splice(HtmlWebpackPluginIdx + 1, 0, new HtmlWebpackInlineSourcePlugin())
-        }
-    }
+    config = rewireInlinSource(config, env, {
+        inlineSource: '.(css)$',
+    })
 
     return config
 }
