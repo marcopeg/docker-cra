e0181898580d0f3ffbe94a1b6d604049bdf8ca8e
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-04 09:24:18 +0200

    Avoid to flush app state if js is disabled, closes #12

--- services/webapp/config-overrides.js
+++ services/webapp/config-overrides.js
@@ -2,7 +2,7 @@
 
 const rewireEslint = require('react-app-rewire-eslint')
 const rewireStylus = require('react-app-rewire-stylus-modules')
-const rewireInlinSource = require('react-app-rewire-inline-sourcep')
+const rewireInlinSource = require('react-app-rewire-inline-source')
 
 module.exports = function override (config, env) {
     config = rewireEslint(config, env)
