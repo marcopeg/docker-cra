8d9e2d28c213c24f6e897e5b96edc56c52cc8082
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-06 17:20:01 +0200

    Export statiRender to create-react-app-ssr

--- services/webapp/src/index.ssr.js
+++ services/webapp/src/index.ssr.js
@@ -3,71 +3,13 @@
         import/prefer-default-export: off
 */
 
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import createHistory from 'history/createMemoryHistory'
 import Root from 'app/containers/Root'
 import RootStatic from 'app/containers/RootStatic'
 import { createStore } from 'app/store'
+import { createSSRRender } from 'create-react-app-ssr'
 
-const renderInitialState = ({
-    url,
-    store,
-    history,
-    timeout,
-    userAgent,
-}) => new Promise((resolve) => {
-    const { ssr } = store.getState()
-
-    const timer = setTimeout(() => {
-        console.error('SSR TIMEOUT', url) // eslint-disable-line
-        resolve()
-    }, timeout)
-
-    // rendering loop, used to resolve nested componentWillMount
-    // data loading side effects.
-    function tick () {
-        renderToString(<Root store={store} history={history} userAgent={userAgent} />)
-        if (!ssr.checkStack()) {
-            clearTimeout(timer)
-            resolve()
-        } else {
-            ssr.once('complete', tick)
-        }
-    }
-
-    history.push(url)
-    renderToString(<Root store={store} history={history} userAgent={userAgent} />)
-    ssr.once('complete', tick)
+export const staticRender = createSSRRender({
+    createStore,
+    ClientApp: Root,
+    StaticApp: RootStatic,
 })
-
-export const staticRender = async (url, initialState = {}, settings = {}) => {
-    const history = createHistory()
-    const { store, events } = createStore(history, initialState)
-    const context = {}
-    const userAgent = settings.userAgent || 'unknown user agent'
-
-    await renderInitialState({
-        url,
-        store,
-        history,
-        events,
-        timeout: settings.timeout || 3000,
-        userAgent,
-    })
-
-    const html = renderToString(<RootStatic store={store} url={url} context={context} userAgent={userAgent} />)
-
-    return {
-        html,
-        context,
-        initialState: {
-            ...store.getState(),
-            ssr: null,
-        },
-    }
-}
-
-// create-react-app-ssr staticRender version signature
-// this is used to check for compatibility with the ssr library
-staticRender.ssrVersion = '0.0.2'
