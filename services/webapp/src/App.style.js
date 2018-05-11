bdc41d000cf53f114a89f44b1e387a27b7295ee9
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-11 17:39:21 +0200

    Add version number into the page

--- services/webapp/src/App.style.js
+++ services/webapp/src/App.style.js
@@ -1,7 +1,15 @@
+import { fonts } from 'styles'
 
 const getStyles = () => ({
     wrapper: {
         marginTop: 80,
+    },
+    version: {
+        ...fonts.text,
+        color: '#aaa',
+        position: 'fixed',
+        bottom: 10,
+        right: 10,
     },
 })
 
